dac58b611fa2eccb1eb8d25ae759cb4cf201cfbb
chenqq@qbm360 <chenqq@qbm360>
2018-05-31 16:56:15 +0800

    add demo

--- README.md
+++ README.md
@@ -31,26 +31,26 @@ API
 ====================================
 
 ```js
-// 示例
+// 示例 具体可参考demo/dateTimePicker.vue
 dateTimePicker.open({
-	value: '',//必选,选中的值，格式为yyyy-MM-dd HH:mm，当value为空，默认选中当前时间，当value不为空，选中value的值
-	max: '',//可选，日期最大值，默认2099-12-31 23:59
-	min: '',//可选，日期最小值，默认1900-12-31 00:00
-	title: '',//可选，标题的文案，默认为空
-	titleColor: '',//可选，默认为空，title不为空时有效，颜色值（#313131）
-	confirmTitle: '', //确认按钮的文案，默认值（完成）
-	confirmTitleColor: '', //确认按钮的文字颜色，默认值(#00b4ff)
-	cancelTitle: '', //取消按钮的文案，默认值（取消）
-	cancelTitleColor: '', //取消按钮的文字颜色，默认值(#313131)
+  value: '',//必选,选中的值，格式为yyyy-MM-dd HH:mm，当value为空，默认选中当前时间，当value不为空，选中value的值
+  max: '',//可选，日期最大值，默认2099-12-31 23:59
+  min: '',//可选，日期最小值，默认1900-12-31 00:00
+  title: '',//可选，标题的文案，默认为空
+  titleColor: '',//可选，默认为空，title不为空时有效，颜色值（#313131）
+  confirmTitle: '', //确认按钮的文案，默认值（完成）
+  confirmTitleColor: '', //确认按钮的文字颜色，默认值(#00b4ff)
+  cancelTitle: '', //取消按钮的文案，默认值（取消）
+  cancelTitleColor: '', //取消按钮的文字颜色，默认值(#313131)
 },(res) =>{//回调
-	//result{string}：success，cancel
-	//data {string}：格式为yyyy-MM-dd HH:mm
-	
-	if(res.result === "success"){
-	    //业务逻辑
-	}else{
-	    //业务逻辑
-	}
+  //result{string}：success，cancel
+  //data {string}：格式为yyyy-MM-dd HH:mm
+
+  if(res.result === "success"){
+    //业务逻辑
+  }else{
+    //业务逻辑
+  }
 });
 
 ```
---
+++ demo/dateTimePicker.vue
@@ -0,0 +1,122 @@
+<template> 
+    <div>
+        <wxc-cell title="默认"
+            :has-top-border="true">
+          <text @click="showNormalDate" v-if="normalDateText">选择时间</text>
+          <text @click="showNormalDate" v-if="normalDateVal">{{normalDate}}</text>
+        </wxc-cell>
+        <wxc-cell title="设置时间可选范围"
+            :has-top-border="true" >
+          <text @click="showStartDate" v-if="startDateText">开始时间</text>
+          <text @click="showStartDate" v-if="startDateVal">{{startDate}}</text>
+          <text>-</text>
+          <text @click="showStartDate" v-if="startDateText">结束时间</text>
+          <text @click="showStartDate" v-if="startDateVal">{{startDate}}</text>
+        </wxc-cell>
+    </div>
+</template>
+<script>
+import { WxcCell } from 'weex-ui';
+
+const dateTimePicker = weex.requireModule('dateTimePicker');
+
+import Title from '../_mods/title';
+export default {
+    components: { WxcCell, Title }, 
+    data () {
+        return {
+            normalDateText: true,
+            normalDateVal: false,
+            normalDate: '',
+            startDateText: true,
+            startDateVal: false,
+            startDate: '',
+            endDateText: true,
+            endDateVal: false,
+            endDate: '',
+        }
+    },
+    methods: {
+        showNormalDate (){
+            dateTimePicker.open({
+                value: this.normalDate,
+            },(res) =>{
+                if(res.result === 'success'){
+                    this.normalDateText = false;
+                    this.normalDateVal = true;
+                    this.normalDate = res.data;
+                }else if(res.result === 'cancel'){
+                    this.$notice.toast({
+                        message: '取消'
+                    });
+                }else{
+                    this.$notice.toast({
+                        message: 'error'
+                    });
+                }
+            });
+        },
+        showStartDate (){
+            dateTimePicker.open({
+                value: this.startDate,
+                max: this.endDate,
+                min: '2018-01-01 00:00',
+                title: '请选择开始时间',
+                titleColor: '#808080',
+                confirmTitle: '确认', 
+                confirmTitleColor: '#FF0000', 
+                cancelTitle: '取消',
+                cancelTitleColor: '#000080',
+            },(res) =>{
+                if(res.result === 'success'){
+                    this.startDateText = false;
+                    this.startDateVal = true;
+                    this.startDate = res.data;
+                }else if(res.result === 'cancel'){
+                    this.$notice.toast({
+                        message: '取消'
+                    });
+                }else{
+                    this.$notice.toast({
+                        message: 'error'
+                    });
+                }
+            });
+        },
+        showEndDate (){//结束时间
+            if(this.startDate === ''){
+                this.$notice.toast({
+                    message: '请先选择开始时间'
+                })
+            }else{
+                dateTimePicker.open({
+                    value: this.endDate,
+                    max: '',
+                    min: this.startDate,
+                    title: '请选择结束时间',
+                    titleColor: '#808080',
+                    confirmTitle: '确认', 
+                    confirmTitleColor: '',
+                    cancelTitle: '',
+                    cancelTitleColor: '',
+                },(res) =>{
+                    if(res.result === 'success'){
+                        this.endDateText = false;
+                        this.endDateVal = true;
+                        this.endDate = res.data;
+                    }else if(res.result === 'cancel'){
+                        this.$notice.toast({
+                            message: '取消'
+                        });
+                    }else{
+                        this.$notice.toast({
+                            message: 'error'
+                        });
+                    }
+                });
+            }
+        },
+    }
+}
+</script>
+
